f91dd2b3f386d75703766a68b5081bd2ab1ea7fd
WarPromo <kooltyme1@gmail.com>
2023-01-24 10:41:50 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const socket = io('https://api.drawevolver.com');
+const socket = io('https://api.drawevolver.com', { transports : ['websocket'] });
 
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
